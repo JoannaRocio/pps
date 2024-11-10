7de18f2deffd82a82d21912cca284e4155f31488
lazarodrigo <lazaro.rodrigo@hotmail.com>
2024-11-10 08:37:53 -0300

    funcion enter

--- Views/Login/login_view.py
+++ Views/Login/login_view.py
@@ -12,13 +12,18 @@ class UsuarioView:
         # Campos de inicio de sesión
         self.username_entry = ctk.CTkEntry(self.root, placeholder_text="Usuario")
         self.username_entry.pack(pady=10)
-
+        self.username_entry.bind("<Return>", self.on_enter_pressed) #acepta el enter
+        
         self.password_entry = ctk.CTkEntry(self.root, show='*', placeholder_text="Contraseña")
         self.password_entry.pack(pady=10)
-
+        self.password_entry.bind("<Return>", self.on_enter_pressed) #acepta el enter
         login_button = ctk.CTkButton(self.root, text="Iniciar Sesión", command=self.login)
         login_button.pack(pady=20)
-        prueba
+        
+    def on_enter_pressed(self, event):
+        # Llama a la función de login cuando se presiona Enter en cualquiera de los campos
+        self.login()
+        
     def login(self):
         username = self.username_entry.get()
         password = self.password_entry.get()
