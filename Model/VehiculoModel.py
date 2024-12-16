befdb1014c8f827529fbbcc6dfc9dab84b544fec
lazarodrigo <lazaro.rodrigo@hotmail.com>
2024-12-15 23:44:13 -0300

    lista compañias

--- Model/VehiculoModel.py
+++ Model/VehiculoModel.py
@@ -8,11 +8,26 @@ class VehiculoModel:
         cursor = self.connection.cursor(dictionary=True)
         cursor.execute("SELECT * FROM vehiculos WHERE cliente_id = %s", (cliente_id,))
         return cursor.fetchall()
+    
+    def obtener_compania(self):
+        try:
+            query = "SELECT id_compania, nombre, sitio_web, estado FROM companias"
+            companias = self.db_connection.fetch_data(query)
+            
+            nombres_companias = [compania[1] for compania in companias]  # compania[1] es el nombre
+            
+            return nombres_companias
+        except Exception as e:
+            print(f"Error al obtener compañías: {str(e)}")
+            return []
 
-    def agregar_vehiculo(self, cliente_id, marca, modelo, anio, tipo_vehiculo, tipo_categoria, vencimiento_poliza, patente, compania_id):
-        cursor = self.connection.cursor()
-        cursor.execute("""
-            INSERT INTO vehiculos (cliente_id, marca, modelo, anio, tipo_vehiculo, tipo_categoria, vencimiento_poliza, patente, compania_id)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", 
-            (cliente_id, marca, modelo, anio, tipo_vehiculo, tipo_categoria, vencimiento_poliza, patente, compania_id))
-        self.connection.commit()
+
+    def agregar_vehiculo(self, marca, modelo, anio, patente, compania_id, tipo_vehiculo, tipo_categoria, accesorios, vencimiento_poliza):
+        
+        query = """ INSERT INTO vehiculos ( marca, modelo, anio, patente, compania_id, tipo_vehiculo, tipo_categoria, accesorios, vencimiento_poliza)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            
+        valores = (marca, modelo, anio, patente, compania_id, tipo_vehiculo, tipo_categoria, accesorios, vencimiento_poliza)
+        
+        self.db_connection.execute_query(query, valores)
+        print("ok")
